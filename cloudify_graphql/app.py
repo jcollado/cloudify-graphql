e6fa7104c30836418621bd208b7e2e0dc3e3e6f1
Javier Collado <javier@gigaspaces.com>
2017-06-16 17:58:21 +0200

    Add GraphQL endpoint
    For now only a ping query can be sent

--- cloudify_graphql/app.py
+++ cloudify_graphql/app.py
@@ -2,10 +2,29 @@
 
 """Application module."""
 
+import graphene
+
 from flask import Flask
+from flask_graphql import GraphQLView
+
+
+class Query(graphene.ObjectType):
+    """Main GraphQL query."""
+    ping = graphene.String()
+
+    def resolve_ping(self, args, context, info):
+        """Return ping response."""
+        return 'pong'
 
 
 def create_app():
     """Create flask application."""
     app = Flask(__name__)
+
+    schema = graphene.Schema(query=Query)
+    app.add_url_rule(
+        '/graphql',
+        view_func=GraphQLView.as_view('graphql', schema=schema, graphiql=True),
+    )
+
     return app
--- setup.py
+++ setup.py
@@ -13,6 +13,8 @@ with open('HISTORY.rst') as history_file:
 
 requirements = [
     'Flask',
+    'Flask-GraphQL',
+    'graphene',
 ]
 
 setup_requirements = [
