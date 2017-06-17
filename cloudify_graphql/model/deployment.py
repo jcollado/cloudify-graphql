28c2755c28fac3532236642bc8e58e10aab392f7
Javier Collado <javier@gigaspaces.com>
2017-06-17 04:55:37 +0200

    Add blueprint field to deployment object

--- cloudify_graphql/model/deployment.py
+++ cloudify_graphql/model/deployment.py
@@ -3,11 +3,24 @@
 """Deployment module."""
 
 import graphene
+import iso8601
+import requests
+
+from flask import current_app as app
+from requests.auth import HTTPBasicAuth
+
+from cloudify_graphql.model.blueprint import Blueprint
 
 
 class Deployment(graphene.ObjectType):
     """A deployment."""
-    blueprint_id = graphene.String(description='Blueprint ID')
+    blueprint = graphene.Field(
+        Blueprint,
+        description='The blueprint the deployment is based on',
+    )
+    blueprint_id = graphene.String(
+        description='The ID of the blueprint the deployment is based on',
+    )
     created_at = graphene.types.datetime.DateTime(
         description='Time when the deployment was created')
     created_by = graphene.String(
@@ -18,3 +31,36 @@ class Deployment(graphene.ObjectType):
         description='The tenant that owns the deployment')
     updated_at = graphene.types.datetime.DateTime(
         description='Time when the deployment was last updated at')
+
+    def resolve_blueprint(self, args, context, info):
+        """"Get blueprint the deployment is based on."""
+        url = 'http://{}/api/v3/blueprints'.format(app.config['MANAGER_IP'])
+        headers = {
+            'Tenant': app.config['TENANT'],
+        }
+        params = {
+            'id': self.blueprint_id,
+        }
+        response = requests.get(
+            url,
+            auth=HTTPBasicAuth(app.config['USER'], app.config['PASSWORD']),
+            headers=headers,
+            params=params,
+        )
+        blueprint_data = response.json()['items'][0]
+        blueprint = Blueprint(
+            created_at=(
+                iso8601.parse_date(blueprint_data['created_at'])
+                if blueprint_data['created_at']
+                else None
+            ),
+            description=blueprint_data['description'],
+            id=blueprint_data['id'],
+            main_file_name=blueprint_data['main_file_name'],
+            updated_at=(
+                iso8601.parse_date(blueprint_data['updated_at'])
+                if blueprint_data['updated_at']
+                else None
+            ),
+        )
+        return blueprint
